16c55674c05a3d9d8f57e45e2d56315ede34ca2e
Bob Zhao <ybzhao1989@gmail.com>
2019-07-08 18:44:29 +0800

    Add two sum brute force, solved at about July 1st

---
+++ leetcode/two-sum.cxx
@@ -0,0 +1,33 @@
+/*
+1. Two Sum
+Easy
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+Accepted
+1,914,081
+Submissions
+4,321,067
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (auto i = nums.begin(); i != nums.end(); i++) {
+            for (auto j = i+1; j != nums.end(); j++) {
+                if (*i + *j == target)
+                    return { i - nums.begin(), j - nums.begin() };
+            }
+        }
+        return {};
+    }
+};
