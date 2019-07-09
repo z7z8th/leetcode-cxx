1a213e3c059e35ecc997da78e9b31e39d0b3747c
Bob Zhao <ybzhao1989@gmail.com>
2019-07-09 17:31:34 +0800

    Add single number, solved by XOR

---
+++ leetcode/single-number.cxx
@@ -0,0 +1,35 @@
+/* 136. Single Number
+Easy
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+Accepted
+484,405
+Submissions
+799,173
+ */
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = 0;
+        for (int i=0; i<nums.size(); i++) {
+            ret ^= nums[i];
+        }
+        return ret;
+    }
+};
