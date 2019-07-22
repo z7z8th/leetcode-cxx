51b1f439d01828516864a0400e38b751031587e8
Bob Zhao <ybzhao1989@gmail.com>
2019-07-22 12:10:04 +0800

    remove nth node from end of list, 4ms.

---
+++ leetcode/remove-nth-node-from-end-of-list.cxx
@@ -0,0 +1,138 @@
+/* 19. Remove Nth Node From End of List
+Medium
+
+Given a linked list, remove the n-th node from the end of list and return its head.
+
+Example:
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+Note:
+
+Given n will always be valid.
+
+Follow up:
+
+Could you do this in one pass?
+Accepted
+418,530
+Submissions
+1,216,115
+ */
+/* Definition for singly-linked list.
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	void removeNodeFromList(ListNode*& node) {
+		ListNode *tmp = node;
+		node = node->next;
+		tmp->next = nullptr;
+		delete tmp;
+	}
+	int depthFromEndAndRemove(ListNode*& node, int n) {
+		if (!node)
+			return 0;
+		int depth = depthFromEndAndRemove(node->next, n) + 1;
+		if (depth == n)        
+			removeNodeFromList(node);
+		return depth;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+		int depth = depthFromEndAndRemove(head, n) + 1;
+		return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+/* 
+./objs/remove-nth-node-from-end-of-list <<EOF
+[1,2,3,4,5]
+5
+EOF
+ */
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        getline(cin, line);
+        int n = stringToInteger(line);
+        
+        ListNode* ret = Solution().removeNthFromEnd(head, n);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
