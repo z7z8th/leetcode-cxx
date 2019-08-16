3e95c6b1f16d363c5034961c414bb73ed6c9cd48
Bob Zhao <ybzhao1989@gmail.com>
2019-08-16 14:11:10 +0800

    binary tree inorder traversal, add TreeNode definition.

--- leetcode/binary-tree-inorder-traversal.cxx
+++ leetcode/binary-tree-inorder-traversal.cxx
@@ -25,15 +25,14 @@ Submissions
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+/* Definition for a binary tree node. */
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 /* Recursive */
 class Solution {
 public:
