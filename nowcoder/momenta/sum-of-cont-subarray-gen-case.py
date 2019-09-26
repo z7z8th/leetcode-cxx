a9a72c836adafbd0a911347d563527cdbaa4ddac
Bob Zhao <ybzhao1989@gmail.com>
2019-09-26 15:21:28 +0800

    sum of cont subarray, tune, < 1s. 1. Nowcoder truncated the test case to 7KB, can't show complete case. 2. Added python script to gen large test case.

---
+++ nowcoder/momenta/sum-of-cont-subarray-gen-case.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+import random
+
+if len(sys.argv) < 3:
+	print('usage: %s n x', sys.argv[0])
+
+n = int(sys.argv[1])
+x = int(sys.argv[2])
+print(n, x)
+rand1 = random.SystemRandom()
+for i in range(n):
+	print(rand1.randrange(1, 100), end=' ')
+print()
