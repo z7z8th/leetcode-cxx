46415dcbd0694a9c3166f80c99bfebf1c1d40e4b
Bob Zhao <ybzhao1989@gmail.com>
2019-09-27 15:09:50 +0800

    max k multiset, add nowcoder slightly faster but simpler sample solution.

--- nowcoder/momenta/max-k-multiset.cxx
+++ nowcoder/momenta/max-k-multiset.cxx
@@ -189,3 +189,52 @@ int main()
 724
 
 #endif
+
+#if 0
+// sample solution from nowcoder, slightly faster than my solution
+// see comment below
+
+#include<iostream>
+#include<map>
+#include<algorithm>
+using namespace std;
+ 
+int main() 
+{
+    int n, k, x;
+    cin >> n >> k;
+    map<int, int> Map;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        Map[x]++;
+    }
+    if (k == 1)
+    {
+        cout << Map.size() << endl;
+    }
+    else
+    {
+        int num;
+        for (map<int, int>::iterator pt = Map.begin(); pt != Map.end(); pt++)
+        {
+            x = (pt->first)*k;
+            if (Map.find(x) == Map.end())
+            {
+                continue;
+            }
+            if (pt->second == 0 || Map[x] == 0)
+            {
+                continue;
+            }
+            num = min(pt->second, Map[x]);
+            n = n - num;  // remove k multiple cnt
+            Map[x] = Map[x] - num;  // remove k multiple cnt
+        }
+        cout << n << endl;
+    }
+ 
+    return 0;
+}
+
+#endif
